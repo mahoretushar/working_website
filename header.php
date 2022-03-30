da7f693e2e3f73eefce7b7aade9745c2a64bd7c1
Tushar Mahore <mahoretushar@gmail.com>
2022-03-30 18:37:36 +0530

    Another Commit

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <?php
 ?>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="description" content="Orbitor,business,company,agency,modern,bootstrap4,tech,software">
     <meta name="author" content="themefisher.com">
